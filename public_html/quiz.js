1aa65753f4f48969b5410a85ebf998dfe5146758
Saeid <snourian@concord.org>
2015-05-05 15:55:15 -0400

    Move hotspot location information from simbuilding to quiz module

--- public_html/quiz.js
+++ public_html/quiz.js
@@ -2,6 +2,8 @@ var score = 0;
 var hotspot = -1;
 var quizInProgress = false;
 var alreadyAnswered = [];
+var hotSpotsRoot;
+var hotSpotsHidden;
 
 function answer(userAnswer) {
     var expectedAnswer;
@@ -140,6 +142,78 @@ function updateQuiz() {
                 $("#quizAlreadyChecked").show();
         }
     }
+}
+
+function initHotspots() {
+    hotSpotsRoot = new THREE.Object3D();
+    scene.add(hotSpotsRoot);
+    var hotSpotsVisible = new THREE.Object3D();
+    hotSpotsRoot.add(hotSpotsVisible);
+    hotSpotsHidden = new THREE.Object3D();
+    hotSpotsRoot.add(hotSpotsHidden);
+
+    var geom = new THREE.SphereGeometry(0.1, 20, 20);
+    var whiteMaterial = new THREE.MeshBasicMaterial();
+
+    initHotspotSingle(0, 4, 6, -5.3, geom, whiteMaterial);
+    initHotspotSingle(1, 14.5, 0.8, -2.8, geom, whiteMaterial);
+    initHotspotSingle(2, 1.5, 1.2, -3.61, geom, whiteMaterial);
+    // Windows
+    initHotspotSingle(6, 4.4, 1.6, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 6.45, 1.6, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 10.53, 1.6, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 12.57, 1.6, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 4.4, 4.8, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 6.45, 4.8, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 8.45, 4.8, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 10.53, 4.8, 4.2, geom, whiteMaterial);
+    initHotspotSingle(6, 12.57, 4.8, 4.2, geom, whiteMaterial);
+    // Baseboard
+    initHotspotSingle(8, 3.15, 0.4, 3.5, geom, whiteMaterial);
+    initHotspotSingle(7, 3.7, 0.4, -0.19, geom, whiteMaterial);
+    initHotspotSingle(7, 13.85, 0.4, 0.5, geom, whiteMaterial);
+    initHotspotSingle(8, 13.85, 0.4, -4.7, geom, whiteMaterial);
+    initHotspotSingle(7, 13, 0.4, -0.25, geom, whiteMaterial);
+
+    initHotspotSingle(8, 3.15, 3.45, 3.5, geom, whiteMaterial);
+    initHotspotSingle(7, 13.85, 3.45, 0, geom, whiteMaterial);
+    initHotspotSingle(8, 13.85, 3.45, -4.7, geom, whiteMaterial);
+    // Wall
+    initHotspotSingle(9, 3.15, 2.5, 2, geom, whiteMaterial);
+    initHotspotSingle(10, 13.85, 2.5, 2, geom, whiteMaterial);
+    initHotspotSingle(19, 4.4, 0.6, 4.2, geom, whiteMaterial);
+    initHotspotSingle(20, 6.45, 0.6, 4.2, geom, whiteMaterial);
+    initHotspotSingle(21, 10.53, 0.6, 4.2, geom, whiteMaterial);
+    initHotspotSingle(22, 12.57, 0.6, 4.2, geom, whiteMaterial);
+
+    // Under kitchen sink
+    initHotspotSingle(12, 2.9, 0.7, -4.8, geom, whiteMaterial);
+    // Tub
+    initHotspotSingle(14, 2.1, 3.8, -4.3, geom, whiteMaterial);
+    // Ceiling
+    initHotspotSingle(16, 6.45, 5.8, 2, geom, whiteMaterial);
+    initHotspotSingle(16, 11.5, 5.7, 2.5, geom, whiteMaterial);
+    // Attic hatch
+    initHotspotSingle(17, 10, 5.8, -1, geom, whiteMaterial);
+
+
+    var shadeMaterial = new THREE.MeshPhongMaterial();
+    shadeMaterial.emissive = new THREE.Color(0x555555);
+    var hotSpot = new THREE.Mesh(new THREE.CylinderGeometry(0.1, 0.1, 0.03, 20, 1), shadeMaterial);
+    hotSpot.userData.id = 3;
+    hotSpot.position.set(8.2, 3, 1);
+    hotSpotsVisible.add(hotSpot);
+    var hotSpot = new THREE.Mesh(new THREE.CylinderGeometry(0.1, 0.1, 0.03, 20, 1), shadeMaterial);
+    hotSpot.userData.id = 4;
+    hotSpot.position.set(10, 3, -2.8);
+    hotSpotsVisible.add(hotSpot);
+}
+
+function initHotspotSingle(id, x, y, z, geometry, material) {
+    var hotSpot = new THREE.Mesh(geometry, material);
+    hotSpot.userData.id = id;
+    hotSpot.position.set(x, y, z);
+    hotSpotsHidden.add(hotSpot);
 }
 
 function toggleQuizQuestion() {
