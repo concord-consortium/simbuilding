0b78c3e38b6c4e47209f1b363219aeb3cdb1f8a5
Saeid Nourian <snourian@concord.org>
2016-03-14 10:41:51 -0400

    Improve tutorial conclusion text

--- public_html/tutorial.js
+++ public_html/tutorial.js
@@ -1,4 +1,4 @@
-/* global camControl */
+/* global camControl, hotSpotsHidden */
 
 var step;
 var tutorial = [
@@ -10,9 +10,8 @@ var tutorial = [
     "The doors of the house will open automatically when you get close to them. Walk toward the front door, wait for it to open, then walk inside the house.",
     "There is a red toolbox on the lower right corner of your screen. Click on it to open.",
     "There are 3 tools available to you. To learn more about them, hover your mouse over each of them and read their description.",
-    "Select IR Camera tool by clicking on it. When IR Camera is on, you can see white HotSpots on various parts of the house such as walls and windows. Find one such HotSpot and point the camera to it.",
-    "You can now see the IR image of this part of the house on the IR Camera screen. On left side you see a question along with 3 possible answers. If you are not sure about the answer, click on the Hint button for additional information. When you're ready, click on an answer. If you're correct, the score will increase by one. Otherwise the score will be reduced.",
-    "Congratulations you have successfully completed the tutorial. Now continue the game by walking around the house and discovering new HotSpots. Good luck!"
+    "Select IR Camera tool by clicking on it. When IR Camera is on, you can see HotSpots on various parts of the house such as walls and windows. Find one such HotSpot and point the camera to it.",
+    "You can now see the IR image of this part of the house on the IR Camera screen. On left side you see a question along with 3 possible answers. If you are not sure about the answer, click on the Hint button for additional information. When you're ready, click on an answer. If you're correct, the score will increase by one. Otherwise the score will be reduced."
 ];
 
 function startTutorial() {
@@ -23,6 +22,7 @@ function startTutorial() {
     $("#tutorial").html(tutorial[step]);
     $("#tutorial").delay(2000).fadeIn();
     startGame();
+    tutorial.push("Congratulations you have successfully completed the tutorial. Now continue the game by walking around the house and discovering new HotSpots. There are " + hotSpotsHidden.children.length + " HotSpots in this house. Good luck!");
 }
 
 function tutorialStep(finishedStep) {
@@ -34,7 +34,7 @@ function tutorialStep(finishedStep) {
                 $("#tutorial").html(tutorial[step]);
                 $("#tutorial").fadeIn();
                 if (step === tutorial.length - 1)
-                    $("#tutorial").delay(5000).fadeOut();
+                    $("#tutorial").delay(15000).fadeOut();
                 n();
             });
         }
@@ -46,7 +46,7 @@ function popupToolDescription(tool) {
         $("#tutorialToolDescription").hide();
     else {
         if (tool === 0)
-            $("#tutorialToolDescription").html("IR Camera:<br/>This tool allows you to see the infrared images of various parts of the house. The red/yellow shade indicates a hot surface and the purple/blue shade indicates a cold surface. Use it to identify in which areas the cold outside air is leaking into the house.");
+            $("#tutorialToolDescription").html("IR Camera:<br/>This tool allows you to see the infrared images of various parts of the house. The red/yellow shade indicates a warmer surface and the purple/blue shade indicates a cooler surface. Use it to identify in which areas the cold outside air is leaking into the house.");
         else if (tool === 1)
             $("#tutorialToolDescription").html("Moisture Meter:<br/>This tool allows you to see the moisture level of various parts of the house. 100% means fully wet and 0% means fully dry. Use it to identify in which areas there is moisture on the surface or inside of the walls from leaks.");
         else if (tool === 2)
